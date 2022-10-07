8093c608d1f9823217a4d77fe6a8a5b7cd635cea
Kathleen Mercado <kathleenmerc@gmail.com>
2022-10-06 22:12:18 -0400

    Added number 5

--- W2D1-Javascript-Functions-Lab.js
+++ W2D1-Javascript-Functions-Lab.js
@@ -1,9 +1,9 @@
 // W2D1 Homework 
 
 //1. 
-// (completed above) Define a function, as a function declaration, maxOfTwoNumbers that takes two numbers as arguments and returns the largest of them. If they are the same, return that number. Use the if-else construct or a ternary expression - the Math.max method is not allowed.
+// Define a function, as a function declaration, maxOfTwoNumbers that takes two numbers as arguments and returns the largest of them. If they are the same, return that number. Use the if-else construct or a ternary expression - the Math.max method is not allowed.
 
-//Solution 1a.
+//Solution 1a. (already given solution)
 // function maxOfTwoNumbers(x, y) {
 //     if (x >= y) {
 //       return x;
@@ -18,6 +18,8 @@
 //   console.log(maxOfTwoNumbers(3, 9));
 
 //Solution 1b.
+console.log("Number 1 Solution:")
+
 function maxOfTwoNumbers (num1, num2) {
     if (num1 >= num2) {
         return num1
@@ -25,13 +27,13 @@ function maxOfTwoNumbers (num1, num2) {
         return num2
     }
 }
-console.log("Number 1 Solution:")
 
 console.log(maxOfTwoNumbers(7,6))
 
 
 //2.
 // Define a function, as a function expression, maxOfThree that takes three numbers as arguments and returns the largest of them. Again, the Math.max method is not allowed.
+console.log("Number 2 Solution:")
 
 const maxOfThree = (x, y, z) => {
     if ((x > y && x > z) || (x === y && x > z) || (x === z && x > y)) {
@@ -46,8 +48,6 @@ const maxOfThree = (x, y, z) => {
         return x
     }
 }
-
-console.log("Number 2 Solution:")
 
 console.log("Below will return x:")
 console.log(maxOfThree(5,4,3))
@@ -67,8 +67,10 @@ console.log(maxOfThree(15,18,18))
 console.log("Below will return same number if x = y = z:")
 console.log(maxOfThree(5,5,5))
 
+
 //3. 
 // Define a function, as a function declaration, isCharAVowel that takes a character as an argument and returns true if it is a vowel, false otherwise.
+console.log("Number 3 Solution:")
 
 function isCharAVowel (character) {
     let lowerCaseCharacter = character.toLowerCase()
@@ -79,7 +81,6 @@ function isCharAVowel (character) {
     }
 }
 
-console.log("Number 3 Solution:")
 console.log(isCharAVowel("a"))
 console.log(isCharAVowel("E"))
 console.log(isCharAVowel("i"))
@@ -87,8 +88,10 @@ console.log(isCharAVowel("O"))
 console.log(isCharAVowel("u"))
 console.log(isCharAVowel("x"))
 
+
 //4.
 // Define a function, as a function expression, sumArraythat takes an array of numbers and returns the sum of those numbers. For example, sumArray([2, 4, 5]);would return 11.
+console.log("Number 4 Solution:")
 
 let sumArray = (arrayInput) => {
     let base = 0
@@ -98,25 +101,34 @@ let sumArray = (arrayInput) => {
     return base
 }
 
-console.log("Number 4 Solution:")
 console.log(sumArray([2,4,5]))
-
 
 
 //5.
 // Define a function, as a function declaration, multiplyArray that takes an array of numbers and returns the product of those numbers. For example, multiplyArray([2, 4, 5]);would return 40.
+console.log("Number 5 Solution:")
 
+function multiplyArray (arrayNumbers) {
+    let baseTwo = 1
+    arrayNumbers.forEach((num) => {
+        baseTwo = baseTwo * num
+    })
+    return baseTwo
+}
+
+console.log(multiplyArray([2, 4, 5]))
 
 
 //6.
-// Define a function, as a function expression, numArgsthat returns the number of arguments passed to the function when called.
-
-//6.
-// Define a function, as a function declaration, reverseStringthat takes a string, reverses the characters, and returns it. For example, reverseString('rockstar');would return the string "ratskcor".
+// Define a function, as a function expression, numArgs that returns the number of arguments passed to the function when called.
 
 
 //7.
-// Define a function, as a function expression, longestStringInArraythat takes an array of strings as an argument and returns the length of the longest string.
+// Define a function, as a function declaration, reverseStringthat takes a string, reverses the characters, and returns it. For example, reverseString('rockstar');would return the string "ratskcor".
+
 
 //8.
+// Define a function, as a function expression, longestStringInArraythat takes an array of strings as an argument and returns the length of the longest string.
+
+//9.
 // Define a function, as a function declaration, stringsLongerThanthat takes an array of strings and a number as arguments, and returns an array of the strings that are longer than the number passed in. For example, stringsLongerThan(['say', 'hello', 'in', 'the', 'morning'], 3);would return ["hello", "morning"].
